fad6f7dfa38d8ae938500725c31f8c7ac9f1b211
Seneca <flabbergasted242@gmail.com>
2021-07-07 14:41:26 +0300

    Initial Commit

---
+++ ViewConstraintsTask2.1lesson/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ViewConstraintsTask2.1lesson
+//
+//  Created by Дмитрий Дмитрий on 07.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
